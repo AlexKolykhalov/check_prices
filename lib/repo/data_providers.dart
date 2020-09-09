fbc5c480efdf81df2d8ac5ed9fbc8e1470384712
Колыхалов Алексей Викторович <C:\Users\asu7\AppData\Roaming\The Bat!>
2020-09-09 09:08:18 +0300

    in mixin add static func

--- lib/repo/data_providers.dart
+++ lib/repo/data_providers.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http_client;
 
 ///Описывает общий request GET с конструкцией try catch
 mixin Requests {
-  _get({Uri url, Map<String, String> headers, String shop}) async {
+  static getHttp({Uri url, Map<String, String> headers}) async {
     try {
       http_client.Response response = await http_client
           .get(url, headers: headers)
@@ -29,9 +29,9 @@ class DataProvider extends Product with Requests {
     Uri urlSearch = Uri.parse('https://lenta.com/api/v1/search?value=$search');
     String cookie = ';lentaT2=lpc; Store=0148; CityCookie=lpc';
 
-    var response = await _get(url: urlDomain, headers: headers, shop: 'lenta');
+    var response = await Requests.getHttp(url: urlDomain, headers: headers);
     headers['Cookie'] = response.headers['set-cookie'] + cookie;
-    response = await _get(url: urlSearch, headers: headers, shop: 'lenta');
+    response = await Requests.getHttp(url: urlSearch, headers: headers);
     var result = jsonDecode(response.body)['skus'];
 
     return mapProducts(result: result, shop: 'lenta');
@@ -42,7 +42,7 @@ class DataProvider extends Product with Requests {
     Uri urlSearch = Uri.parse(
         'https://api.metro-cc.ru/api/v1/C98BB1B547ECCC17D8AEBEC7116D6/39/suggestions?query=$search');
 
-    var response = await _get(url: urlSearch, headers: headers, shop: 'metro');
+    var response = await Requests.getHttp(url: urlSearch, headers: headers);
     var result = jsonDecode(response.body)['data']['topProducts'];
 
     return mapProducts(result: result, shop: 'metro');
@@ -55,11 +55,11 @@ class DataProvider extends Product with Requests {
     Uri urlSearch =
         Uri.parse('https://5ka.ru/api/v2/special_offers/?search=$search');
 
-    var response = await _get(url: urlDomain, headers: headers, shop: '5ka');
+    var response = await Requests.getHttp(url: urlDomain, headers: headers);
     headers['Cookie'] =
         'location_id=1871; Path=/,' + response.headers['set-cookie'];
 
-    response = await _get(url: urlSearch, headers: headers, shop: '5ka');
+    response = await Requests.getHttp(url: urlSearch, headers: headers);
     var result = jsonDecode(utf8.decode(response.bodyBytes))['results'];
 
     return mapProducts(result: result, shop: '5ka');
